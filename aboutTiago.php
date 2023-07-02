f3b0bc17900e545bfe95530e56339338dd95aa6f
TiagSant <tiago.santos@emailswiss.ch>
2023-07-02 22:45:10 +0200

    [WTS-20] aboutTiago page created + text moved

---
+++ aboutTiago.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title><?php echo _("À propos de Tiago Santos ?") ?></title>
+
+   <link rel="stylesheet" href="./css/style.css">
+
+</head>
+
+<body class="darkPurple">
+   <div class="darkPurple intro">
+      <h1><?php echo _("Qui est Tiago ?") ?></h1>
+      <br>
+      <p><?php echo _("Je me présente : ") ?><br>
+      <br>
+      <?php echo _("Mon nom est Tiago Santos, je suis électronicien de niveau technicien, 
+      pationné d'informatique, titulaire du brevet fédéral (Suisse) de technicien 
+      audiovisuel et chef de projet.") ?><br>
+      <br>
+      <?php echo _("Toutes mes études ont eu lieu en Suisse, principalement à Genève. Pendant 
+      mes études d'électronicien, j'ai découvert la programmation en C pour les 
+      microcontrolleurs (80C51) pour faire mes prototypes. Ensuite, j'ai appris le C++ 
+      pour faire de l'accès matériel entre mes prototypes électroniques et l'ordinateur, 
+      c'était du Windows XP.") ?><br>
+      <br>
+      <?php echo _("Durant mes études d'électronique, j'ai participé à deux reprises au concours 
+      de robotique Eurobot 2003 et 2004. J'ai vraiment aimé cet expérience.") ?><br>
+      <br>
+      <?php echo _("J'ai commencé ma carrière professionnelle dans une entreprise de luminaires de 
+      secours en tant qu'électronicien pendant 3 ans avant de rejoindre Fischer Connectors 
+      où j'ai participé à l'élaboration des configurations de l'appareil de test électrique 
+      d'assamblage de câble. Dans cette entreprise, j'ai également énormément appris sur la 
+      fibre optique. C'est grâce à ces connaissances, que j'ai pu intégrer la Radio Télévision 
+      Suisse (RTS) en tant que câbleur avant de passer chargé de projet technique en infrastructure 
+      audiovisuelle.") ?></p>
+   </div>
+</body>
+
+</html>
