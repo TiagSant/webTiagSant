aae5fbb4763fde3f892d2076014e00985f6879d8
TiagSant <tiago.santos@emailswiss.ch>
2023-07-03 15:55:10 +0200

    [WTS-19] Main page + CSS file rename

--- aboutTiago.php
+++ aboutTiago.php
@@ -7,7 +7,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title><?php echo _("À propos de Tiago Santos ?") ?></title>
 
-   <link rel="stylesheet" href="./css/style.css">
+   <link rel="stylesheet" href="./css/global.css">
 
 </head>
 
--- index.php
+++ index.php
@@ -4,8 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TiagSant</title>
+
+    <link rel="stylesheet" href="/css/global.css">
+
 </head>
-<body>
-    
+<body class="darkPurple">
+    <div class="intro">
+        <h1><?php echo _("The purpose of the website") ?></h1><br>
+        <p><?php echo _("The main goal of this website is to share knowledge between
+        tech lovers. It can be electronic, coding, cybersecurity, computer science and networking.")?>
+        <br>
+        <br>
+        <?php echo _("Anyone who wants to share his knowledge can express it and can be a editor.") ?></p>
+    </div>
 </body>
 </html>
